18031a9bea4bd61138da48c8f098e8a2d233ea2b
Daniel <roghoh@gmail.com>
2023-09-16 17:47:15 +0400

    Add pages with new class BasePage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Pytest_project
+This is a repository for storing homework for the test automation course on the Stepik platform.
+Starting from lesson 4.1.6
--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,15 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 
-def test_presence_of_btn_that_add_item_to_cart(browser):
-    browser.get(link)
-    assert browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+# def test_presence_of_btn_that_add_item_to_cart(browser):
+#     browser.get(link)
+#     assert browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
